99af65e3de859b307c78fb43c883f7df118afd62
hasnatulalam <hasnatualam056@gmail.com>
2023-10-23 21:26:35 +0600

    .envfile created

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express =require('express');
-const {PORT}=require('./config/serverConfig');
+const bodyParser =require('body-parser')
+const { PORT }=require('./config/serverConfig');
+const apiRoutes =require('./routes/index')
 const app = express();
 
 
 const prepareAndStartServer =()=>{
 app.listen(PORT,()=>{
-    console.log(`Server Start at ${PORT}`)
+      app.use(bodyParser);
+      app.use(bodyParser.urlencoded({extended:true}));
+     app.use('/api',apiRoutes);
+     console.log(`server started at ${PORT}`);
 })
 }
 prepareAndStartServer()
